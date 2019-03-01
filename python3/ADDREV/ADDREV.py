c12a66011213345c49c9f29d48bc1c2a061099b9
Daniel <dan@fedoraWS.am.dxccorp.net>
2019-03-01 10:45:01 -0600

    changing funct name

--- python3/ADDREV/ADDREV.py
+++ python3/ADDREV/ADDREV.py
@@ -5,10 +5,10 @@ def reverse( user_in ):
 
 	return str(int(inp_arr[0].strip('0')[::-1]) + int(inp_arr[1].strip('0')[::-1])).strip('0')[::-1]
 
-def addrev():
+def main():
 	reps = int(input())
 	for i in range(reps):
 		print(reverse(input()))
 
 if __name__ == "__main__":
-	addrev()	
+	main()	
