bbf021ed930ea2e69c67eab67aae6b5548cdc9c8
Daniel Felt <knightfelt@gmail.com>
2019-03-15 14:56:35 -0500

    fixing up prime funct

--- python3/Euler_Problems/utils.py
+++ python3/Euler_Problems/utils.py
@@ -3,17 +3,21 @@
 import functools
 
 def is_prime(n):
-    """"pre-condition: n is a nonnegative integer
+    """"pre-condition: n is an integer
     post-condition: return True if n is prime and False otherwise."""
-    if n < 2: 
-         return False;
-    if n % 2 == 0:             
-         return n == 2  # return False
-    k = 3
-    while k*k <= n:
-         if n % k == 0:
-             return False
-         k += 2
+    if n < 2: return False
+    if n == 2: return True
+    if n == 3: return True
+    if n % 2 == 0: return False
+    if n % 3 == 0: return False
+    
+    i = 5
+    w = 2
+
+    while i * i <= n:
+        if n % i == 0: return False
+        i += w
+        w = 6 - w
     return True
 
 
