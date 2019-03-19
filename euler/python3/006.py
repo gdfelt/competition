7b661342afea198ebc886cce747ecd2d6392d7a1
gdfelt <knightfelt@gmail.com>
2019-03-19 10:51:50 -0500

    fixing up problem 6

--- euler/python3/006.py
+++ euler/python3/006.py
@@ -17,4 +17,14 @@ Find the difference between the sum of the squares of the first one
 hundred natural numbers and the square of the sum.
 """
 
+def main():
+	sum_sqr = 0
+	sqr_sum = 0
+	for x in range(1, 100+1):
+		sum_sqr += x
+		sqr_sum += x ** 2
+	sum_sqr = sum_sqr ** 2
+	print(sum_sqr - sqr_sum)
 
+if __name__ == "__main__":
+	main()
