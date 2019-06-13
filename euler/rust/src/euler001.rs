ee12cb9dfa52843eb668c4922e9c35588e213966
Daniel Felt <dfelt2@dxc.com>
2019-06-13 09:44:26 -0500

    first problem in rust

--- euler/rust/src/euler001.rs
+++ euler/rust/src/euler001.rs
@@ -1,20 +1,24 @@
-pub mod euler001 {
     
-    pub fn main() -> i32 {
-        3 
-    }
-
-    #[cfg(test)]
-    mod tests {
-        
-        use super::*;
-
-        #[test]
-        fn test_add() {
-            assert_eq!(main(), 3);
-        }
-    }
-
+pub fn main() -> i32 {
+    sum_div_by(3) + sum_div_by(5) - sum_div_by(15)   
 }
+
+fn sum_div_by(num:i32) -> i32 {
+    let target = 999;
+    let p = target / num;
+    num * ( p * (  p + 1 )) / 2
+}
+
+#[cfg(test)]
+mod tests {
+    
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(main(), 233168);
+    }
+}
+
 
 
