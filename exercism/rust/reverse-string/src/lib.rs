a17541000cd57a4d295805efbaad643c0c622b60
dan <dfelt3@dxc.com>
2020-04-06 12:13:52 -0500

    cleanup

--- exercism/rust/reverse-string/src/lib.rs
+++ exercism/rust/reverse-string/src/lib.rs
@@ -1,8 +1,7 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    // unimplemented!("Write a function to reverse {}", input);
-    // let word: &str = input;
+
     input.graphemes(true).rev().collect()
 
 }
