689ff9994af971865185716a7cbc3dd6ba6df5ed
dan <dfelt3@dxc.com>
2020-04-08 09:11:40 -0500

    revisions

--- exercism/rust/gigasecond/src/lib.rs
+++ exercism/rust/gigasecond/src/lib.rs
@@ -1,7 +1,8 @@
 use chrono::{DateTime, Duration, Utc};
 
+const GIGASEC_IN_SECS: i64 = 1000000000;
+
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    let dt = start + Duration::seconds(1000000000);
-    dt
+    return start + Duration::seconds(GIGASEC_IN_SECS);
 }
