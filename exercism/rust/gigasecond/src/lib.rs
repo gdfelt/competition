2d63bbfb24a88d9b0a22d751fc41935a75cfce07
dan <dfelt3@dxc.com>
2020-04-08 10:10:32 -0500

    more refinement

--- exercism/rust/gigasecond/src/lib.rs
+++ exercism/rust/gigasecond/src/lib.rs
@@ -4,5 +4,5 @@ const GIGASEC_IN_SECS: i64 = 1000000000;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    return start + Duration::seconds(GIGASEC_IN_SECS);
+    start + Duration::seconds(GIGASEC_IN_SECS)
 }
