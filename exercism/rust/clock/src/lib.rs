c26d7cc8b3bd0d3cbed1cd384200782e47c9f8ef
dan <dfelt3@dxc.com>
2020-05-01 09:54:18 -0500

    refactor based on feedback

--- exercism/rust/clock/src/lib.rs
+++ exercism/rust/clock/src/lib.rs
@@ -8,29 +8,38 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let clock: Clock = Clock {
-            hours: 0,
-            minutes: 0,
+
+        let mut curr_time: i32 = (hours * 60 + minutes) % 1440;
+        
+        if curr_time < 0 {
+            curr_time += 1440
         }
-        .add_minutes(hours * 60 + minutes);
-        clock
+
+        Clock {
+            hours: (curr_time / 60) % 24,
+            minutes: curr_time % 60
+        }
+
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let mut clock: Clock = *self;
-        // Convert time to number of minutes and add minute modifications
-        let mut curr_time: i32 = (clock.hours * 60 + clock.minutes) + (minutes % 1440);
 
-        // Wrap around if negative
-        if curr_time < 0 {
-            curr_time = curr_time + 1440
-        }
+        let tmp_clock: Clock = *self;
+        Clock::new(tmp_clock.hours, tmp_clock.minutes + minutes)
+        // let mut clock: Clock = *self;
+        // // Convert time to number of minutes and add minute modifications
+        // let mut curr_time: i32 = (clock.hours * 60 + clock.minutes) + (minutes % 1440);
 
-        // Convert back to hrs, mins
-        clock.hours = (curr_time / 60) % 24;
-        clock.minutes = curr_time % 60;
+        // // Wrap around if negative
+        // if curr_time < 0 {
+        //     curr_time = curr_time + 1440
+        // }
 
-        clock
+        // // Convert back to hrs, mins
+        // clock.hours = (curr_time / 60) % 24;
+        // clock.minutes = curr_time % 60;
+
+        // clock
     }
 }
 
