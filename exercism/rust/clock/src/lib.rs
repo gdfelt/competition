c41e5580ac4b940fb380d0413501989f3aa9790a
dan <dfelt3@dxc.com>
2020-05-01 09:56:45 -0500

    removing commented code

--- exercism/rust/clock/src/lib.rs
+++ exercism/rust/clock/src/lib.rs
@@ -26,20 +26,7 @@ impl Clock {
 
         let tmp_clock: Clock = *self;
         Clock::new(tmp_clock.hours, tmp_clock.minutes + minutes)
-        // let mut clock: Clock = *self;
-        // // Convert time to number of minutes and add minute modifications
-        // let mut curr_time: i32 = (clock.hours * 60 + clock.minutes) + (minutes % 1440);
-
-        // // Wrap around if negative
-        // if curr_time < 0 {
-        //     curr_time = curr_time + 1440
-        // }
-
-        // // Convert back to hrs, mins
-        // clock.hours = (curr_time / 60) % 24;
-        // clock.minutes = curr_time % 60;
-
-        // clock
+        
     }
 }
 
