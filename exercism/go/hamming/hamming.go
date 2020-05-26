810fe40c64e1ca6af4843d81a6ac4fb082510e34
dan <dfelt3@dxc.com>
2020-05-26 12:07:22 -0500

    hamming solution

--- exercism/go/hamming/hamming.go
+++ exercism/go/hamming/hamming.go
@@ -1,4 +1,23 @@
 package hamming
 
+import (
+	e "errors"
+)
+
 func Distance(a, b string) (int, error) {
+
+	if len(a) != len(b) {
+		return -1, e.New("Strings are not equal")
+	}
+
+	count := 0
+
+	for i, _ := range a {
+		if a[i] != b[i] {
+			count += 1
+		}
+	}
+
+	return count, nil
+
 }
