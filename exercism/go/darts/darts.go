cabb2557ea41f0168db8eb36efa30279f89b1d2c
dan <dfelt3@dxc.com>
2020-05-26 13:50:35 -0500

    working darts

--- exercism/go/darts/darts.go
+++ exercism/go/darts/darts.go
@@ -1,1 +1,19 @@
 package darts
+
+import "math"
+
+// Score funct
+func Score(x float64, y float64) int {
+
+	dist := math.Abs(math.Sqrt(x*x + y*y))
+
+	if dist <= 1 {
+		return 10
+	} else if dist <= 5 {
+		return 5
+	} else if dist <= 10 {
+		return 1
+	}
+	return 0
+
+}
