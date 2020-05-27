7b277b661751c573e6a73b1b065d6a5700d3f365
dan <dfelt3@dxc.com>
2020-05-27 11:32:07 -0500

    working raindrops solution

--- exercism/go/raindrops/raindrops.go
+++ exercism/go/raindrops/raindrops.go
@@ -1,1 +1,28 @@
 package raindrops
+
+import (
+	"strconv"
+)
+
+// Convert produces a 'Raindrop' string based on the factors of the number provided
+func Convert(number int) string {
+
+	result := ""
+	if number%3 == 0 {
+		result += "Pling"
+	}
+
+	if number%5 == 0 {
+		result += "Plang"
+	}
+
+	if number%7 == 0 {
+		result += "Plong"
+	}
+
+	if result == "" {
+		return strconv.Itoa(number)
+	}
+	return result
+
+}
