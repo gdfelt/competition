8c530e27a8de9109eedf655fab1dbd2b46926bb8
dan <dfelt3@dxc.com>
2020-06-03 10:53:29 -0500

    working nucleotide solution

--- exercism/go/nucleotide-count/nucleotide_count.go
+++ exercism/go/nucleotide-count/nucleotide_count.go
@@ -1,30 +1,26 @@
+// Package dna produces a histogram of valid nucleotides
 package dna
 
 import (
-	"fmt"
+	"errors"
 )
 
 // Histogram is a mapping from nucleotide to its count in given DNA.
-// Choose a suitable data type.
-type Histogram map[string]int
+type Histogram map[int32]int
 
-// DNA is a list of nucleotides. Choose a suitable data type.
+// DNA is a list of nucleotides.
 type DNA string
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
-///
-// Counts is a method on the DNA type. A method is a function with a special receiver argument.
-// The receiver appears in its own argument list between the func keyword and the method name.
-// Here, the Counts method has a receiver of type DNA named d.
 func (d DNA) Counts() (Histogram, error) {
-	var h Histogram
-	fmt.Println(d)
+	var h Histogram = map[int32]int{'A': 0, 'C': 0, 'G': 0, 'T': 0}
+
 	for _, c := range d {
 		if c == 'A' || c == 'C' || c == 'G' || c == 'T' {
 			h[c] = h[c] + 1
 		} else {
-			return nil, error.New("Invalid nucleotide detected: ", c)
+			return nil, errors.New("Invalid nucleotide detected")
 		}
 	}
 
