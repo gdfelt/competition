42a1dd9e37c25b2fc2382cbb0e3428d9de53aa7f
Daniel Felt <knightfelt@gmail.com>
2021-01-10 19:19:49 -0600

    working part1 soln

--- advent_of_code/advent_2020/day_04/day_04.py
+++ advent_of_code/advent_2020/day_04/day_04.py
@@ -1,9 +1,27 @@
 class Day04(object):
     def __init__(self):
-        pass
+        self.data = []
+        with open("advent_2020/day_04/day_04.dat", "r") as file:
+            self.data = file.read().split("\n\n")
+            self.data = [d.replace("\n", " ") for d in self.data]
 
     def part_01(self):
-        return 0
+        count = 0
+        for d in self.data:
+            passport = {}
+            for entry in d.split(" "):
+                passport[entry.split(":")[0]] = entry.split(":")[1]
+            if (
+                "byr" in passport
+                and "iyr" in passport
+                and "eyr" in passport
+                and "hgt" in passport
+                and "hcl" in passport
+                and "ecl" in passport
+                and "pid" in passport
+            ):
+                count += 1
+        return count
 
     def part_02(self):
         return 0
--- advent_of_code/advent_2020/day_04/test_day_04.py
+++ advent_of_code/advent_2020/day_04/test_day_04.py
@@ -8,7 +8,7 @@ class TestAoC:
         return Day04()
 
     def test_day4_part1(self, setup):
-        assert setup.part_01() == 0
+        assert setup.part_01() == 204
 
     def test_day4_part2(self, setup):
         assert setup.part_02() == 0
