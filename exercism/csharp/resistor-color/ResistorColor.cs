6bd2fca090a3b6f28ac214d88a9364b765ce871b
Felt <dfelt3@dxc.com>
2021-02-04 08:44:51 -0600

    working resistor colors soln

---
+++ exercism/csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class ResistorColor
+{
+
+    public static int ColorCode(string color)
+    {
+        int colorCode = -1;
+        switch (color.ToLower())
+        {
+            case "black":
+                colorCode = 0;
+                break;
+            case "orange":
+                colorCode = 3;
+                break;
+            case "white":
+                colorCode = 9;
+                break;
+        }
+        return colorCode;
+    }
+
+    public static string[] Colors()
+    {
+        return new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+    }
+}
