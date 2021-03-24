3a17ffceb6508483f2ec082064426574dc2d9af3
dan <dfelt3@dxc.com>
2021-03-24 17:07:48 -0500

    working phone number solution

--- exercism/go/phone-number/phone_number.go
+++ exercism/go/phone-number/phone_number.go
@@ -1,9 +1,38 @@
 package phonenumber
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"unicode"
+)
 
 func parsePhone(input string) (string, error) {
-	return "3335557777", nil
+
+	s := ""
+
+	for _, r := range input {
+		if unicode.IsNumber(r) {
+			s = s + string(r)
+		}
+	}
+
+	if len(s) != 10 {
+		if len(s) == 11 && s[0] == '1' {
+			s = s[1:]
+		} else {
+			return "", errors.New("Country Code must be One")
+		}
+	}
+
+	if s[0] == '0' || s[0] == '1' {
+		return "", errors.New("Area code cannot start with Zero or One")
+	}
+
+	if s[3] == '0' || s[3] == '1' {
+		return "", errors.New("Exchange code cannot start with Zero or One")
+	}
+
+	return s, nil
 }
 
 func Number(input string) (string, error) {
@@ -27,5 +56,5 @@ func Format(input string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return fmt.Sprintf("(%s) %s-%s", number[:3], number[3:6], number[7:]), nil
+	return fmt.Sprintf("(%s) %s-%s", number[:3], number[3:6], number[6:]), nil
 }
