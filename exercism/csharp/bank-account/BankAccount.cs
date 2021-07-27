8f83c0210a947f3c12f0e6ed5e3b7ea43a3e3b96
dan <dfelt3@dxc.com>
2021-07-27 17:31:35 -0500

    working bank account solution

--- exercism/csharp/bank-account/BankAccount.cs
+++ exercism/csharp/bank-account/BankAccount.cs
@@ -2,26 +2,40 @@
 
 public class BankAccount
 {
+    private readonly object acctLock = new object();
+    private decimal balance = 0.00m;
+    private bool open = false;
+
     public void Open()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // throw new NotImplementedException("You need to implement this function.");
+        open = true;
     }
 
     public void Close()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // throw new NotImplementedException("You need to implement this function.");
+        open = false;
     }
 
     public decimal Balance
     {
         get
         {
-            throw new NotImplementedException("You need to implement this property.");
+            if(open){
+                return balance;
+            } else {
+                throw new InvalidOperationException("Account is closed.");
+            }
+            
         }
     }
 
     public void UpdateBalance(decimal change)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // throw new NotImplementedException("You need to implement this function.");
+        lock(acctLock){
+            balance += change;
+        }
     }
 }
