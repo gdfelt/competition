c23aa5def437a83fecce5beb902d7482cae7d199
Daniel Felt <knightfelt@gmail.com>
2022-04-18 15:14:03 -0500

    working go module

---
+++ advent_of_code/go/Advent2021/2021.go
@@ -0,0 +1,5 @@
+package Advent2021
+
+func Day01p1() int {
+	return 1234
+}
---
+++ advent_of_code/go/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	Advent2021 "main/Advent2021"
+)
+
+func main() {
+	fmt.Println("Out")
+	fmt.Println(Advent2021.Day01p1())
+}
